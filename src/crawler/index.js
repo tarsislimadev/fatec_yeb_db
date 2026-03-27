304edb7ab22fcd8c982650f872f6a6c639de8e65
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-27 08:43:28 -0300

    Update project files

--- src/crawler/index.js
+++ src/crawler/index.js
@@ -1,15 +1,29 @@
-const run = async (cnpj = '47339227000120') => {
+const fs = require('fs')
+
+const state = { companies: [] }
+
+const searchCNPJ = async (cnpj = '83887703000100') => {
   const url = `https://www.econodata.com.br/consulta-empresa?filtro=${cnpj}`
 
-  console.log({ url })
+  console.log({ cnpj, url })
 
   const html = await fetch(url).then(r => r.text())
 
-  console.log({ html })
-
   const cnpjs = html.match(/\d\d\.?\d\d\d\.?\d\d\d\/\d\d\d\d[-]?\d\d/ig)
 
-  console.log({ cnpjs })
+  state.companies = state.companies.concat(cnpjs.filter((cnpj, index) => state.companies.indexOf(cnpj) === index))
 }
 
-run()
+searchCNPJ()
+
+const saveCNPJ = async () => {
+  const cnpj = state.companies.shift()
+
+  const data = await fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj}`).then(r => r.json())
+
+  fs.writeFileSync(`./cnpj.${cnpj}.json`, JSON.stringify(data))
+
+  setTimeout(() => saveCNPJ(), 1500)
+}
+
+setTimeout(() => saveCNPJ(), 1500)
